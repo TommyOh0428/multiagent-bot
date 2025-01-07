56b858834a8a27219d6f024044db37c08dfd8b4f
Tommy Oh <tommy32666@gmail.com>
2025-01-06 16:31:37 -0800

    initial setup

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
 import os
-import discord
-from discord.ext import commands
+from bot import bot
+from app import create_app
 from dotenv import load_dotenv
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv('TOKEN')
 
-# Create a bot instance
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(command_prefix='!', intents=intents)
-
-@bot.event
-async def on_ready():
-    print(f'{bot.user} has connected to Discord!')
-
-def main():
-    bot.run(DISCORD_TOKEN)
-
 if __name__ == '__main__':
-    main()
+    app = create_app()
+    bot.run(DISCORD_TOKEN)
+    app.run(port=5000)
