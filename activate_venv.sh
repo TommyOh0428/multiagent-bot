16dedf04c29e8d41be612c304c89f0b95425912f
Tommy Oh <tommy32666@gmail.com>
2025-02-01 19:13:46 -0800

    fix activate_venv

--- activate_venv.sh
+++ activate_venv.sh
@@ -2,17 +2,21 @@
 
 echo "Activating virtual environment"
 
+if [ ! -d ".venv" ]; then
+    echo "Error: Virtual environment not found. Please create it first."
+    exit 1
+fi
+
+echo "Running on OS: $OSTYPE"
+
 # if macos, use this command
 if [[ "$OSTYPE" == "darwin"* ]]; then
     source ./.venv/bin/activate
-fi
 
 # if windows, use the following command
-if [[ "$OSTYPE" == "msys" ]]; then
+elif [[ "$OSTYPE" == "msys" ]]; then
     .\venv\Scripts\activate
-fi
 
 # if linux, use the following command
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
     source ./.venv/bin/activate
-fi
