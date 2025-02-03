221ff55b1b35e869af2dba722f2111370ac447e3
TommyTroll123 <tommy32666@gmail.com>
2025-02-02 18:14:34 -0800

    add print statement that venv activated

--- activate_venv.sh
+++ activate_venv.sh
@@ -12,12 +12,15 @@ echo "Running on OS: $OSTYPE"
 # if macos, use this command
 if [[ "$OSTYPE" == "darwin"* ]]; then
     source ./.venv/bin/activate
+    echo "Activated virtual environment"
 fi
 # if windows, use the following command
 if [[ "$OSTYPE" == "msys" ]]; then
     .\venv\Scripts\activate
+    echo "Activated virtual environment"
 fi
 # if linux, use the following command
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     source ./.venv/bin/activate
+    echo "Activated virtual environment"
 fi
