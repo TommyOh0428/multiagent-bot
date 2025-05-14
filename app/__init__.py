adb7e4c1961edc7e8d9408527faecb6d879b138d
Tommy Oh <tommy32666@gmail.com>
2025-05-14 16:01:01 -0700

    fixing import error

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 import os
 import discord
 from discord.ext import commands
-from agents.scrum_master_agent import ScrumMasterAgent
+from .agents.scrum_master_agent import ScrumMasterAgent
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,7 +9,7 @@ load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
-intents.messages = True
+intents.message_content = True
 
 bot = commands.Bot(command_prefix = "/", intents = intents)
 
@@ -19,5 +19,5 @@ async def on_message(message):
         return
 
     if message.content.startswith("/scrum"):
-        response = ScrumMasterAgent.get_scrum_response(message.content)
+        response = await ScrumMasterAgent.get_scrum_response(message.channel, message.content)
         await message.channel.send(response)
